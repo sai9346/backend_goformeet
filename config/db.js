b39770f7fd41c9c7905839ddb68c9532e1de2683
KARNATAKA SAIKUMAR <81470320+sai9346@users.noreply.github.com>
2024-08-18 17:23:30 +0530

    db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
-const db = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
+      useUnifiedTopology: true,
     });
-    console.log('MongoDB connected...');
-  } catch (err) {
-    console.error('Could not connect to MongoDB...', err);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
